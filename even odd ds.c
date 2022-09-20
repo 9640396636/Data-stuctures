618f1bfede8e640380d6b745b29fe58177bbf61a
9640396636 <113878818+9640396636@users.noreply.github.com>
2022-09-20 13:20:18 +0530

    Add files via upload

---
+++ even odd ds.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+	int a[10],i;
+	printf("enter the values");
+	for (i=0;i<7;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	printf("even number\n");
+	for(i=0;i<7;i++);
+	{
+		if(a[i]%2==0)
+    {
+    	printf("%d\n",a[i]);
+    }
+}
+printf("odd number \n");
+for(i=0;i<7;i++)
+{
+	if(a[i]%2==1){
+		printf("%d\n",a[i]);
+	return 0;
+}
+	
+
+	
+		
